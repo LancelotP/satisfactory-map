ff7cf84f96814f9efeb8ad4eaa21b34e003fba8a
Lancelot Prigent <lancelot.prigent@gmail.com>
2019-03-25 15:36:54 +0100

    ping point 0,0 top left

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,27 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as L from "leaflet";
 import "leaflet-mouse-position";
 import "leaflet/dist/leaflet.css";
 
 export const App = () => {
+  const [latLng, setLatLng] = useState<undefined | L.LatLng>(undefined);
+
   useEffect(printMap, []);
 
   function printMap() {
-    var mapExtent = [0.0, -4000.0, 4000.0, 0.0];
+    var bounds = L.latLngBounds(L.latLng(0, 0), L.latLng(4000, 4000));
     var mapMinZoom = 2;
     var mapMaxZoom = 6;
     var mapMaxResolution = 0.25;
     var mapMinResolution = Math.pow(2, mapMaxZoom) * mapMaxResolution;
-    var tileExtent = [0.0, -4000.0, 4000.0, 0.0];
-    var maxBounds = [[0, 0], [-4000, 4000]];
-    var crs = L.CRS.Simple;
+    const crs = L.CRS.Simple;
+
     // @ts-ignore
-    crs.transformation = new L.Transformation(
-      1,
-      -tileExtent[0],
-      -1,
-      tileExtent[3]
-    );
+    crs.transformation = new L.Transformation(1, -0, 1, 0);
     crs.scale = function(zoom) {
       return Math.pow(2, zoom) / mapMinResolution;
     };
@@ -29,54 +25,132 @@ export const App = () => {
       return Math.log(scale * mapMinResolution) / Math.LN2;
     };
 
-    // @ts-ignore
-    var map = new L.Map("test", {
-      renderer: L.canvas,
-      maxZoom: mapMaxZoom,
-      minZoom: mapMinZoom,
-      // @ts-ignore
-      // layers: overlays,
-      crs: crs,
-      maxBounds: maxBounds,
-      maxBoundsViscosity: 1,
-      attributionControl: false,
-      zoomControl: false
+    var map = L.map("test", {
+      crs: L.CRS.Simple,
+      zoom: 0,
+      center: [0, 0],
+      maxBounds: L.latLngBounds(L.latLng(0, 0), L.latLng(4000, 4000))
     });
 
-    // @ts-ignore
     const layer = L.tileLayer("/tiles/{z}/{x}/{y}.png", {
-      minZoom: mapMinZoom,
-      maxZoom: mapMaxZoom,
-      bounds: [[0, 0], [-4000, 4000]],
-      tms: false
+      minZoom: 0,
+      maxZoom: 6,
+      bounds: [[0, 0], [4000, 4000]]
+      // tms: false
     }).addTo(map);
 
-    map.fitBounds([
-      crs.unproject(L.point(mapExtent[2], mapExtent[3])),
-      crs.unproject(L.point(mapExtent[0], mapExtent[1]))
-    ]);
+    map.fitBounds(bounds);
 
-    //Coordinates Display (Bottom Left)
     // @ts-ignore
-    L.control.mousePosition().addTo(map);
+    map.on("mousemove", ({ latlng }) => setLatLng(L.latLng(latlng)));
 
-    const ironNodeGroup = L.layerGroup();
+    // const ironNodeGroup = L.layerGroup();
 
-    ironNodeGroup.addTo(map);
+    // ironNodeGroup.addTo(map);
 
-    const ironIcon = L.icon({
-      iconUrl:
-        "https://www.conanexilesmap.com/data/images/icons/icon_ironstone.png",
+    // const ironIcon = L.icon({
+    //   iconUrl:
+    //     "https://www.conanexilesmap.com/data/images/icons/icon_ironstone.png",
 
-      iconSize: [32, 32],
-      iconAnchor: [16, 16],
-      popupAnchor: [0, -10]
-    });
+    //   iconSize: [32, 32],
+    //   iconAnchor: [16, 16],
+    //   popupAnchor: [0, -10]
+    // });
 
-    L.marker([-1266.25, 1465], { icon: ironIcon })
-      .bindPopup("Pure icon")
-      .addTo(ironNodeGroup);
+    // L.marker([2000, 2000], { icon: ironIcon })
+    //   .bindPopup("Pure icon")
+    //   .addTo(ironNodeGroup);
   }
 
-  return <div id="test" style={{ height: "100vh" }} />;
+  // console.log(latLng);
+
+  return (
+    <div style={{ position: "relative" }}>
+      <div id="test" style={{ height: "100vh" }} />
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          background: "red",
+          padding: 12,
+          zIndex: 100000
+        }}
+      >
+        {JSON.stringify(latLng)}
+      </div>
+    </div>
+  );
 };
+
+// function printMap() {
+//   var mapExtent = [0.0, -4000.0, 4000.0, 0.0];
+//   var mapMinZoom = 2;
+//   var mapMaxZoom = 6;
+//   var mapMaxResolution = 0.25;
+//   var mapMinResolution = Math.pow(2, mapMaxZoom) * mapMaxResolution;
+//   var tileExtent = [0.0, -4000.0, 4000.0, 0.0];
+//   var maxBounds = [[0, 0], [-4000, 4000]];
+//   var crs = L.CRS.Simple;
+//   // @ts-ignore
+//   crs.transformation = new L.Transformation(
+//     1,
+//     -tileExtent[0],
+//     -1,
+//     tileExtent[3]
+//   );
+//   crs.scale = function(zoom) {
+//     return Math.pow(2, zoom) / mapMinResolution;
+//   };
+//   crs.zoom = function(scale) {
+//     return Math.log(scale * mapMinResolution) / Math.LN2;
+//   };
+
+//   // @ts-ignore
+//   var map = new L.Map("test", {
+//     renderer: L.canvas,
+//     maxZoom: mapMaxZoom,
+//     minZoom: mapMinZoom,
+//     // @ts-ignore
+//     // layers: overlays,
+//     crs: crs,
+//     maxBounds: maxBounds,
+//     maxBoundsViscosity: 1,
+//     attributionControl: false,
+//     zoomControl: false
+//   });
+
+//   // @ts-ignore
+//   const layer = L.tileLayer("/tiles/{z}/{x}/{y}.png", {
+//     minZoom: mapMinZoom,
+//     maxZoom: mapMaxZoom,
+//     bounds: [[0, 0], [-4000, 4000]],
+//     tms: false
+//   }).addTo(map);
+
+//   map.fitBounds([
+//     crs.unproject(L.point(mapExtent[2], mapExtent[3])),
+//     crs.unproject(L.point(mapExtent[0], mapExtent[1]))
+//   ]);
+
+//   //Coordinates Display (Bottom Left)
+//   // @ts-ignore
+//   L.control.mousePosition().addTo(map);
+
+//   const ironNodeGroup = L.layerGroup();
+
+//   ironNodeGroup.addTo(map);
+
+//   const ironIcon = L.icon({
+//     iconUrl:
+//       "https://www.conanexilesmap.com/data/images/icons/icon_ironstone.png",
+
+//     iconSize: [32, 32],
+//     iconAnchor: [16, 16],
+//     popupAnchor: [0, -10]
+//   });
+
+//   L.marker([-1266.25, 1465], { icon: ironIcon })
+//     .bindPopup("Pure icon")
+//     .addTo(ironNodeGroup);
+// }
