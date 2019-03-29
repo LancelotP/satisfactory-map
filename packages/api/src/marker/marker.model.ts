163e1ab132f62735183ea63d1253144990f3c457
Lancelot Prigent <lancelot.prigent@gmail.com>
2019-03-29 15:28:27 +0100

    convert points to float

--- packages/api/src/marker/marker.model.ts
+++ packages/api/src/marker/marker.model.ts
@@ -2,9 +2,9 @@ import { Resource } from "../common/common.model";
 import { Column } from "typeorm";
 
 export class Marker extends Resource {
-  @Column()
+  @Column({ type: "double precision" })
   lat: number;
 
-  @Column()
+  @Column({ type: "double precision" })
   lng: number;
 }
